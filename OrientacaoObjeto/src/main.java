849e60e0072e54ee5a6f1d59b9fa63d36ae635cd
aluno <aluno@DSN1251230712>
2022-04-04 11:26:00 -0300

    Segundo Commit

---
+++ OrientacaoObjeto/src/main.java
@@ -0,0 +1,18 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Pessoa pessoa1 = new Pessoa();
+		
+		pessoa1.nome = "Cardelis";
+		pessoa1.numFigurinhas = 1;
+		
+		pessoa1.receber(3);
+		
+		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.numFigurinhas);
+
+	}
+
+}

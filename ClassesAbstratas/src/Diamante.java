62702c044c7d4977f5c231bf16ae2d3578867331
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-05-17 09:39:01 -0300

    Update Diamante.java

--- ClassesAbstratas/src/Diamante.java
+++ ClassesAbstratas/src/Diamante.java
@@ -1,9 +1,9 @@
 
-public class Diamante extends Item {
+public class Diamante extends Item {  //Herança: extendendo Classe diamante com a Superclasse Item
 
-	@Override
-	public void pegar() {
-		System.out.println("Pegou Diamante");
+	@Override // Sobreescrita: pega o metodo da classe mae e altera o padrao
+	public void pegar() { // Criando o metodo Pegar Diamante
+		System.out.println("Pegou Diamante");// Mostra informação na tela
 		
 	}
 	

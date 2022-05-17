5d484708359edcc65c7cae7f078ca4ee1d251b82
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-05-17 09:48:25 -0300

    Update Moeda.java

--- ClassesAbstratas/src/Moeda.java
+++ ClassesAbstratas/src/Moeda.java
@@ -1,9 +1,9 @@
 
 public class Moeda extends Item{  //Herança: Extendendo a classe Moeda com a superclasse Item
 
-	@Override // Sobreescrita
-	public void pegar() {
-		System.out.println("Pegou Moeda");
+	@Override // Sobreescrita: pega o metodo da classe mae e altera o padrao
+	public void pegar() { //Criando o metodo Pegar Moeda
+		System.out.println("Pegou Moeda"); // Mostra informaçao na tela
 
 
 	}

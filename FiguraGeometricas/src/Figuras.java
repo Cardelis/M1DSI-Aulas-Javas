59d6d2b46c54fbed41ce4c4c1272aaf1b11c24c6
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-05-17 10:25:41 -0300

    Update Figuras.java

--- FiguraGeometricas/src/Figuras.java
+++ FiguraGeometricas/src/Figuras.java
@@ -3,11 +3,11 @@ public class Figuras {
 
 	public static void main(String[] args) {
 		
-		AreaCalculavel triangulo = new Triangulo();
-		triangulo.calcularArea();
+		AreaCalculavel triangulo = new Triangulo(); // Instanciando objeto
+		triangulo.calcularArea(); // 
 		
-		AreaCalculavel circunferencia = new Circunferencia();
-		circunferencia.calcularArea();
+		AreaCalculavel circunferencia = new Circunferencia(); // Instanciando objeto
+		circunferencia.calcularArea();//
 		
 		
 		

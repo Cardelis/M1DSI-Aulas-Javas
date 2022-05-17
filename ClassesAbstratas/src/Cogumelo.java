34c4a8a51e2d898b5a25fe4ee853ea3112fcce19
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-05-17 09:33:49 -0300

    Update Cogumelo.java

--- ClassesAbstratas/src/Cogumelo.java
+++ ClassesAbstratas/src/Cogumelo.java
@@ -1,9 +1,9 @@
 
 public class Cogumelo extends Item {
 
-	@Override
+	@Override // Sobreescrita: pega o metodo da classe mae e altera o padrao
 	public void pegar() {
-		System.out.println("Pegou Cogumelo");
+		System.out.println("Pegou Cogumelo");// Mostra informaçao na tela
 		
 	}
 	

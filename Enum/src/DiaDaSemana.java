4d39d6a5554326032214ce6d1df3d6b7f1539d8f
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-05-23 11:39:28 -0300

    Add files via upload

---
+++ Enum/src/DiaDaSemana.java
@@ -0,0 +1,26 @@
+
+public enum DiaDaSemana { // Tipo de classe enum: constroi valores dentro da RIP. conjunto de valore constantes
+	
+	//
+	DOMINGO(1),// dentro dele vai ter um atributo tipo inteiro chamado NUM asocciado com o parametro
+	SEGUNDA(2),
+	TERCA(3),
+	QUARTA(4),
+	QUINTA(5),
+	SEXTA(6),
+	SABADO(7);
+	
+	private int num; // Atributos nao consegue accesar de fora
+	
+	
+	
+	private DiaDaSemana(int num) {
+	this.num = num; // construtor
+	
+	}
+	public int getNum() {
+		return num;
+	
+	}
+	
+}
---
+++ Enum/src/Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+	
+	public static final double PI = 3.14; // Criando constante(final determina que é constante)
+	
+	private enum Sexo{ // Enum dentro da classe
+		MASCULINO, FEMENINO
+	}
+
+	public static void main(String[] args) {
+		
+		
+		double pi = Main.PI; // acessando a constante static
+		
+		DiaDaSemana hoje = DiaDaSemana.SEGUNDA;     // dentro dela so consigo guardar endereços dos objetos PRONTOS dos enum que estao em dia da semama (so cosegue mostrar endereço dos objeto no enum  
+		
+		int num = DiaDaSemana.SEGUNDA.getNum();
+		System.out.println(num);
+		
+		String s = "TERCA";
+		
+		hoje = DiaDaSemana.valueOf(s); // procura ista de constantes de diadasemana
+		
+		System.out.println(hoje.getNum());
+		
+		Sexo sexo = Sexo.MASCULINO; // 
+		sexo = Sexo.FEMENINO;
+		
+
+	}
+
+}

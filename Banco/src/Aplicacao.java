9a4292dbe51846cacb1b34a99ba08e8c8f369887
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-05-22 21:55:47 -0300

    Update Aplicacao.java

--- Banco/src/Aplicacao.java
+++ Banco/src/Aplicacao.java
@@ -3,11 +3,21 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
-		ContaBancaria c1 = new ContaCorrente();
-		c1.depositar(800);
-		c1.sacar(500);
-		double saldo;
+		ContaCorrente contaCorrente = new ContaCorrente(); // Instanciar Objeto(Da classe ContaCorrente
+		contaCorrente.depositar(200); // depositar 200 reais no saldo conta corrente
+		contaCorrente.transferir(250); // Transferir 250 reais
+		contaCorrente.sacar(1500); // sacar 1000 reais
+		contaCorrente.calcularSaldo(); // Calcular saldo final
+		System.out.println(); // pular linha
+		
+		ContaInvestimento containvestimento = new ContaInvestimento();
+		containvestimento.depositar(60);     //depositar 200 reais no saldo da containvestimento
+		containvestimento.transferir(30);    //transferir 40 reais no saldo da containvestimento
+		containvestimento.sacar(10);         //sacar 200 reais no saldo da containvestimento
+		containvestimento.calcularSaldo();   //calcular saldo final
+	
 
+	}
 	}
 	
 

4850f23fdb25f8cbe9fdf6d970fdb4f7bdeef87b
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-05-22 22:11:43 -0300

    Update ContaCorrente.java

--- Banco/src/ContaCorrente.java
+++ Banco/src/ContaCorrente.java
@@ -1,6 +1,15 @@
 
 public class ContaCorrente extends ContaBancaria{
 	
-	//*SALDO ATUA SUBTRAINDO 10% IMPOSTO QUE DEVE SER PAGO
+			@Override  
+		public double calcularSaldo() {    //override do método calcular saldo da superclasse.
+			double saldofinal;     //variável criada para guardar o valor do saldo final
+
+						saldofinal = getSaldo()*0.90; //fórmula para calcular o saldo menos 10%
+			
+			System.out.println("Seu saldo final na Conta Corrente é R$ " + saldofinal);
+			return saldofinal;
+		}
+
 
 }

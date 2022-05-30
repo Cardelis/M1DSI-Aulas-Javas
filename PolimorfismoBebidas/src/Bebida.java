94c16bb6b067f27c7883f0d19ee617d8e3bc1fd5
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-05-30 10:21:19 -0300

    Update Bebida.java

--- PolimorfismoBebidas/src/Bebida.java
+++ PolimorfismoBebidas/src/Bebida.java
@@ -1,20 +1,23 @@
 
 public class Bebida {
+	
+	//Criando atributos
 	private String nome;
 	private boolean aquecer;
 
-	public Bebida(String nome, boolean aquecer) {
+	public Bebida(String nome, boolean aquecer) { // criando objeto
 		super();
-		this.nome = nome;
-		this.aquecer = aquecer;
+		this.nome = nome; // para mostrar que esse é um atributo da Classe 
+		this.aquecer = aquecer; // para mostrar que esse é um atributo da Classe 
 	}
-	public void preparar() {
+	public void preparar() { // Criando metodo
 		System.out.println("Pegando o copo");
 	
 	}
+	// Metodos GETTERS e SETTERS
 
-	public String getNome() {
-		return nome;
+	public String getNome() { 
+		return nome; 
 	}
 
 	public void setNome(String nome) {

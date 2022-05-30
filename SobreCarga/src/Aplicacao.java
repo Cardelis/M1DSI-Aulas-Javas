57a1ce8946eb2ba30796eb2ce0a0718a76f06f19
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-05-30 09:54:02 -0300

    Update Aplicacao.java

--- SobreCarga/src/Aplicacao.java
+++ SobreCarga/src/Aplicacao.java
@@ -1,10 +1,10 @@
-import java.util.Scanner; // Invocacao de scanner
+import java.util.Scanner; // Importacoes da JRE
 
 public class Aplicacao { // Classe publica aplicacao
 
 	public static void main(String[] args) { // metodo main para permitir a execucao do programado programa
 		
-		Scanner scanner = new Scanner(System.in); // 
+		Scanner scanner = new Scanner(System.in); // classe entanciando objeto, ela permite receber informacao do usuario
 		
 
 		Matematica matematica = new Matematica();// instanciando objeto da classe
@@ -13,21 +13,21 @@ public class Aplicacao { // Classe publica aplicacao
 	
 		System.out.println("Somar dois numeros");// Mostra informacao na tela
 		System.out.println("Digite o Primeiro numero");// Mostra informacao na tela
-		int x = scanner.nextInt();
+		int x = scanner.nextInt(); // Guarda informacao digitada pelo usuario na variavel
 		
 		System.out.println("Digite o segundo numero");// Mostra informacao na tela
-		int y = scanner.nextInt();
+		int y = scanner.nextInt(); // Guarda informacao digitada pelo usuario na variavel
 	
 
-		int soma = matematica.somar(x, y);
+		int soma = matematica.somar(x, y); // Guarda o resutado de MATEMATICA.SOMAR e guarda dentro da variavel
 
 		System.out.println("o resultado é " + soma + "!!!");// Mostra informacao na tela
 
-		soma = matematica.somar(x, y, 5);
+		soma = matematica.somar(x, y, 5); // Guarda o resultado de MATEMATICA.SOMAR dentro de SOMA
 		System.out.println("o resultado é " + soma + "!!!");// Mostra informacao na tela
 		
 		
-		double soma2 = matematica.somar(x, y);
+		double soma2 = matematica.somar(x, y);// Guarda o resultado de MATEMATICA.SOMAR dentro da variavel
 		System.out.println("o resultado é " + soma2 + "!!!");// Mostra informacao na tela
 	}
 

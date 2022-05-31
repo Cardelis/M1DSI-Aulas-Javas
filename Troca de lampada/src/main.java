e36396541c64b597bb98faefd2fb16b90c37c792
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-05-31 19:10:19 -0300

    Update main.java

--- Troca de lampada/src/main.java
+++ Troca de lampada/src/main.java
@@ -3,10 +3,10 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		Lampada lampada = new Lampada();
+		Lampada lampada = new Lampada(); // instanciando objeto da classe
 	
 		
-	
+	// chamando metodos da classe
 	lampada.desligar();
 	lampada.imprimir();
 	

afa21e35b787bd0389da96169236713540c5aa18
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-01 15:50:24 -0300

    Update Gato.java

--- Polimorfismo/src/Gato.java
+++ Polimorfismo/src/Gato.java
@@ -1,9 +1,9 @@
 
-public class Gato extends Animal {
+public class Gato extends Animal { // classe extendendo a superclasse animal
 
-	@Override // é uma forma de garantir que voce esta subrescrevendo um metodo e naao criando um novo
-	public void Falar() {
-		System.out.println("MIAUUUU");
+	@Override // Ã© uma forma de garantir que voce esta subrescrevendo um metodo e naao criando um novo
+	public void Falar() { // metodo falar
+		System.out.println("MIAUUUU"); // mostra nba tela
 	}
 	
 

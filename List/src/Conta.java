4c4e9c946379656d21da6a55c4859bab4f8895aa
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-02 07:42:56 -0300

    Update Conta.java

--- List/src/Conta.java
+++ List/src/Conta.java
@@ -6,11 +6,11 @@ public class Conta {
 
 	public Conta(String nome, double valor) {
 
-		this.nome = nome;
-		this.valor = valor;
+		this.nome = nome; // para mostrar que esse é um atributo da Classe 
+		this.valor = valor; // para mostrar que esse é um atributo da Classe 
 	}
 
-	@Override
+	@Override 
 	public String toString() {
 		return "Conta [nome=" + nome + ", valor=" + valor + "]";
 	}

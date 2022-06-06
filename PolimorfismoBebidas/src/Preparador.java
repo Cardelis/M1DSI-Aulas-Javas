14beada1ec5a3f5d94677ac9c024813054c835fa
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-06 08:42:56 -0300

    Create Preparador.java

--- PolimorfismoBebidas/src/Preparador.java
+++ PolimorfismoBebidas/src/Preparador.java
@@ -1,12 +1,12 @@
 
 public class Preparador {
 
-	public void prepararBebida(Bebida bebida) {
+	public void prepararBebida(Bebida bebida) { // criando metodo preparar bebida
 
-		System.out.println("Preparando a Bebiada" + bebida.getNome());
+		System.out.println("Preparando a Bebiada" + bebida.getNome()); // Mostra na tela
 		
 
-		bebida.preparar();
+		bebida.preparar(); // esta chamando o metodo preparar
 
 		if (bebida.isAquecer()) {  // teste condional
 			System.out.println("Aquecendo a bebida" + bebida.getNome()); // Mostra na tela

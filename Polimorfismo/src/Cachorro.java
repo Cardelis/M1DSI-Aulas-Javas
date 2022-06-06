85045a49c29961999f84475987862b5197a9a775
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-06 08:15:30 -0300

    Update Cachorro.java

--- Polimorfismo/src/Cachorro.java
+++ Polimorfismo/src/Cachorro.java
@@ -5,7 +5,7 @@ public class Cachorro  extends Animal { //Classe cachorro extendendo a superclas
 	public void Falar() { // metodo falar 
 		System.out.println("GUAUUUUUU"); // Mostra informacao na tela
 	}
-	public void Morder() { // mwtodo morder
+	public void Morder() { // metodo morder
 		System.out.println("Miaaaaki"); // mostra informacao na tela
 	}
 	

1451e3fb9b11573067c6446acdfcc238d2c75db7
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-06 08:20:46 -0300

    Update Refrigerante.java

--- PolimorfismoBebidas/src/Refrigerante.java
+++ PolimorfismoBebidas/src/Refrigerante.java
@@ -9,7 +9,7 @@ public class Refrigerante extends Bebida {
 
 	@Override //  é uma forma de garantir que você está sobrescrevendo um método e não criando um novo.
 	public void preparar() {  // metodo preparar
-		super.preparar();
+		super.preparar(); //usado para chamar os métodos e variáveis da Superclasse
 		System.out.println("Colocando gelo no copo"); // mostra informacao na tela
 		System.out.println("Colocando Refrigerante"); // mostra informacao na tela
 	}

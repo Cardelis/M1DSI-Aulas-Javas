82fe8ff1db7baf439998bd062608549d7f0c7926
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-07 13:03:59 -0300

    Update Main.java

--- EscreverTXT/src/Main.java
+++ EscreverTXT/src/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {// classe main chamada para JVM
+public class Main {
 
 	public static void main(String[] args) throws IOException {// intrface IOExcepion: trata o possivl erro que pode dar
 
@@ -23,7 +23,7 @@ public class Main {// classe main chamada para JVM
 		try (BufferedWriter escrever = new BufferedWriter(new FileWriter("saida.txt"))) { // classe responsavel por  guardar inormacao no buffer( bufferwriter)(FileWriter abRe o caminho para o arquivo no HD("try: analisa s da o erro, e abre e fecha as portas que o buffer ta abrindo)
 			for (Mouse mouse : listaDeMouses)
 
-				escrever.write(mouse.toString()); // to string e o metodo que a gente usa para poder subcrever
+				escrever.write(mouse.toString()); // toString: retorna escrito o valor de todas as variaveis da classe
 		}
 
 	}

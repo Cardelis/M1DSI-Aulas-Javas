5bc21cca2350827a134ae3c041e6f7e7fe8852b2
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-07 09:39:31 -0300

    Update Animal.java

--- Polimorfismo/src/Animal.java
+++ Polimorfismo/src/Animal.java
@@ -1,6 +1,8 @@
 
-public class Animal { // Superclasse
+public class Animal { // Animal e uma Superclasse, portanto, as subclasses vao herdar caracteristicas dela.
 
-	public void Falar() { // Metodo falar
+
+	public void Falar() { // // A classe animal possui um metodo generico que e o Falar
+	// Esse metodo pode ser herdado por qualquer classe que herde de animal e possuir suas proprias caracteristicas
 }
 }

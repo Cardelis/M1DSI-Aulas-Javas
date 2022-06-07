2364714ccad14b7477646cf138410cb446d8cb7a
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-07 12:30:06 -0300

    Update Aplicaçao.java

--- Herança/src/Aplicaçao.java
+++ Herança/src/Aplicaçao.java
@@ -3,7 +3,7 @@ public class Aplicaçao { //Super classe
 
 	public static void main(String[] args) {
 
-		Veiculo veiculo = new Veiculo(); // Instanciando objeto
+		Veiculo veiculo = new Veiculo(); // instanciado um objeto veiculo com esse objeto eu consigo chamar os metodos setters para accesar aos atributos criados na classe veiculo
 		
 		
                 
@@ -14,35 +14,35 @@ public class Aplicaçao { //Super classe
 		veiculo.imprimir();
 		veiculo.buzinar();
 
-		Carro carro = new Carro(); // Instanciando objeto
-                // metodo set de carro
+		Carro carro = new Carro(); // Instanciando objeto carro
+                // herda tambem seus metodos e atributos, a mesma coisa vale para a moto e o caminhao
 		carro.setMarca("Chevrolet");
 		carro.setModelo("Aveo");
 		carro.setAno(2012);
 
-		System.out.println();
+		System.out.println(); // pula linha
 
 		carro.imprimir();
 		carro.buzinar();
 
-		Caminhao caminhao = new Caminhao(); // Instanciando objeto
-                // metodo set de caminhao
+		Caminhao caminhao = new Caminhao(); 
+               
 		caminhao.setMarca("SCANIA");
 		caminhao.setModelo("XJT76");
 		caminhao.setAno(2021);
 
-		System.out.println();
+		System.out.println(); // pula linha
 
 		caminhao.imprimir();
 		caminhao.buzinar();
 		
-		Moto moto = new Moto(); // Instanciando objeto
+		Moto moto = new Moto();
 		
 		moto.setMarca("Honda");
 		moto.setModelo("Remaza");
 		moto.setAno(2015);
 		
-		System.out.println();
+		System.out.println(); // pula linha
 		
 		moto.imprimir();
 		moto.buzinar();

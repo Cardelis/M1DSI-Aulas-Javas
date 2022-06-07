c631609b864801e952542a3fdfd1ccdb0831a449
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-07 09:53:01 -0300

    Update Main.java

--- Polimorfismo/src/Main.java
+++ Polimorfismo/src/Main.java
@@ -3,26 +3,26 @@ public class Main extends Animal { // classe main extendida a superclasse Animal
 
 	public static void main(String[] args) {
 
-		Animal animal = new Cachorro();  
-		animal.Falar();
+		Animal animal = new Cachorro(); // instaciando objeto do tipo da classe
+		animal.Falar(); // metodo falar
 
-		animal = new Gato();
-		animal.Falar();
+		animal = new Gato();  // instaciando objeto do tipo da classe
+		animal.Falar(); //  metodo falar
 
-		Gato gato = new Gato();
+		Gato gato = new Gato(); // instanciando objeto da classe
 		Cachorro cachorro = new Cachorro();// instanciando objeto da classe
-		falar(gato);
-		falar(cachorro);
+		falar(gato); // chamo o metodo falar que vem do objeto gato
+		falar(cachorro);  // chamo o metodo falar que vem do objeto cachorro
 	}
 
 	public static void falar(Animal a) {
 		a.Falar();
 		
-		if(a instanceof Cachorro) {
+		if(a instanceof Cachorro) { // insctanceof verifica se o objeto pertence a classe que eu estou comparando
 			
-			Cachorro c = (Cachorro) a;
+			Cachorro c = (Cachorro) a; //  casting explicito : forcei o objeto cachorro a chamar o metodo morder
 			
-		c.Morder();
+		c.Morder(); // metodo Morder
 		}
 
 

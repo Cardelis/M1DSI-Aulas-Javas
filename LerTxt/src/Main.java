8062791010a4e8dd77f67a573a8d24de95c7ec8b
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-07 12:23:13 -0300

    Update Main.java

--- LerTxt/src/Main.java
+++ LerTxt/src/Main.java
@@ -1,3 +1,4 @@
+// Importacoes da JRE
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -6,21 +7,21 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		try(BufferedReader reader = new BufferedReader(new FileReader("entrada.tt"))){
-			String line;
-			String string = "";
+		try(BufferedReader reader = new BufferedReader(new FileReader("entrada.tt"))){ //O try vai tentar ler o arquivo por meio do BufferedReader
+			String line; // foi definida para receber uma linha no arquivo txt
+			String string = ""; // vai receber o conteudo da line dar um \n para separar o conteudo e nao misturar tudo
 			
 			while((line = reader.readLine()) != null)  { // While: ele vai procurar a linha certa procurando linha por linha
 				string += line + "\n";
 			}
-			System.out.println(string);
+			System.out.println(string); // Mostra na tela o resultado obtido
 				
 			}
-		String teste = "Mouse: funciona= sim, qtdBotoes= 2, tamanho = pequeno\n ";
+		String teste = "Mouse: funciona= sim, qtdBotoes= 2, tamanho = pequeno\n "; // String que recebe os atributos do Mouse
 		
-		Mouse mouse = new Mouse(teste);
+		Mouse mouse = new Mouse(teste); // instanciando mouse que vai receber como parametro a variavel teste que criamos
 		
-		System.out.println(mouse);
+		System.out.println(mouse); // moatra na tela o resultado dos algaritmos de acima
 		}
 		
 		

316f1a23909e4534c79cd0a20946c660ea38acc8
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-07 16:54:44 -0300

    Update Carro.java

--- Interface/src/Carro.java
+++ Interface/src/Carro.java
@@ -1,6 +1,6 @@
 
-public interface Carro extends Automovel {
+public interface Carro extends Automovel {  //Classe Carro extendendo a superclasse Automovel
 	
-	public void AbrirPorta();
+	public void AbrirPorta(); // foi criado um metodo que e usado para carro e nao para todos os automoveis 
 
 }

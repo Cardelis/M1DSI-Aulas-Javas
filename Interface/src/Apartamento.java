2270c9c9ef7d299eb407963b0a7932e839b7d1fb
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-07 16:30:07 -0300

    Update Apartamento.java

--- Interface/src/Apartamento.java
+++ Interface/src/Apartamento.java
@@ -1,9 +1,12 @@
 
-public class Apartamento implements ItemCaro {
+public class Apartamento implements ItemCaro { // A classe apartamento faz parte de item caro 
+	
+	@Override //  é uma forma de garantir que você está sobrescrevendo um método e não criando um novo.
+	
+	public double getPreco() { // Metodo herdado 
+		
+		return 250000; // Mas possui seu valor especifico
 
-	@Override
-	public double getPreco() {
-		return 250000;
 	}
 	
 	

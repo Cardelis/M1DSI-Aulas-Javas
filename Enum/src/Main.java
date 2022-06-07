0f0fcae8fbef2e2c75944f2c9bdcc337c852b9b1
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-07 13:12:16 -0300

    Update Main.java

--- Enum/src/Main.java
+++ Enum/src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
 	
-	public static final double PI = 3.14; // Criando constante(final determina que é constante)
+	public static final double PI = 3.14; // Criando constante(final determina que Ã© constante)
 	
 	private enum Sexo{ // Enum dentro da classe
 		MASCULINO, FEMENINO
@@ -10,20 +10,20 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		double pi = Main.PI; // acessando a constante static
+		double pi = Main.PI; // instanciando uma variavel
 		
-		DiaDaSemana hoje = DiaDaSemana.SEGUNDA;     // dentro dela so consigo guardar endereços dos objetos PRONTOS dos enum que estao em dia da semama (so cosegue mostrar endereço dos objeto no enum  
+		DiaDaSemana hoje = DiaDaSemana.SEGUNDA; //Instanciando objeto da classe DiaDaSemana e passa SEGUNDA como parametro
 		
-		int num = DiaDaSemana.SEGUNDA.getNum();
+		int num = DiaDaSemana.SEGUNDA.getNum(); //  Instanciando variavel do tipo int. O getNum vai atribuir um numero o dia da semana, conforme feito na classe Enu
 		System.out.println(num);
 		
 		String s = "TERCA"; // criando variavel para chamar parametro do emum
-		
+		// O objeto hoje, criado acima, e convertido para String a fim de receber o dia da semana
 		hoje = DiaDaSemana.valueOf(s); // procura lista de constantes de DiaDaSemana
 		
 		System.out.println(hoje.getNum());
-		
-		Sexo sexo = Sexo.MASCULINO; // 
+		//chamar um Enum utilizando um objeto
+		Sexo sexo = Sexo.MASCULINO;
 		sexo = Sexo.FEMENINO;
 		
 

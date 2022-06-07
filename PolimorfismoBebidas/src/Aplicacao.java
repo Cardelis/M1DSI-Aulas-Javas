a5655cab2cf3f28e435b6f0df7eaf05bf02a7e8a
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-07 09:27:25 -0300

    Update Aplicacao.java

--- PolimorfismoBebidas/src/Aplicacao.java
+++ PolimorfismoBebidas/src/Aplicacao.java
@@ -3,13 +3,13 @@ public class Aplicacao {
 
 	public static void main(String[] args) { // Metodo main para permitir a execucao do programa no console
 
-		Preparador preparador = new Preparador(); // Intanciando objeto da classe
+		Preparador preparador = new Preparador(); // Intanciando a classe
 
 		LeiteQuente leiteQuente = new LeiteQuente();// Intanciando objeto da classe
-		preparador.prepararBebida(leiteQuente); //preparando a bebida , realizando um metodo em um obejeto
+		preparador.prepararBebida(leiteQuente); //chamando o metodo preparar bebida
 
 		Refrigerante refrigerante = new Refrigerante();// Intanciando objeto da classe
-		preparador.prepararBebida(refrigerante); //preparando a bebida , realizando um metodo em um obejeto
+		preparador.prepararBebida(refrigerante); ////chamando o metodo preparar bebida
 	}
 
 }

c1ff1adb990fd1929a925adf93e641f89306cf74
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-07 12:32:08 -0300

    Update Caminhao.java

--- Herança/src/Caminhao.java
+++ Herança/src/Caminhao.java
@@ -2,8 +2,8 @@
 public class Caminhao extends Veiculo { // extendendo a subclasse com a super classe VEICULO
 	
 
-	@Override
-	public void buzinar() { // criando metodo buzinar 
+	@Override //  é uma forma de garantir que você está sobrescrevendo um método e não criando um novo.
+	public void buzinar() { 
 		System.out.println("FOOOOM FOOOOOOOM"); // Mostra informacao na tela
 	}
 

76bf6cacb2c468836d2aaab429bfc7bf5aba851d
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-07 12:35:06 -0300

    Update Carro.java

--- Herança/src/Carro.java
+++ Herança/src/Carro.java
@@ -1,11 +1,11 @@
 
-public class Carro extends Veiculo {
+public class Carro extends Veiculo { // Classe cachorro extendendo a superclasse Animal
 	
-	private boolean quatroPortas;
+	private boolean quatroPortas; // criando variavel privada
 
-	@Override
+	@Override //  é uma forma de garantir que você está sobrescrevendo um método e não criando um novo.
 	public void imprimir() {
-		super.imprimir();
+		super.imprimir(); // super herda todo o codigo que esta dentro do metodo imprimir
 		System.out.println("Tem 4 Portas:" + quatroPortas);
 	}
 	public boolean isQuatroPortas() {

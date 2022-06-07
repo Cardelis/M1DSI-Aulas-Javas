e19a4ac9860bbc68fb0fb4fbe0e6f4f97317c3ef
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-07 16:46:56 -0300

    Update Automovel.java

--- Interface/src/Automovel.java
+++ Interface/src/Automovel.java
@@ -1,6 +1,7 @@
 
 public interface Automovel {
 	
+	//  instanciando metodos genericos na interface automovel que podem ser acessados por qualquer classe que implementa a interface
 	void virarEsquerda();
 	void virarDireita();
 	void acelera();

38b29ecd3d2ecd05775f209e1c8ac758b54e3d62
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-08 19:15:39 -0300

    Add files via upload

---
+++ ClassesAbstratas/src/Cogumelo.java
@@ -0,0 +1,8 @@
+
+public class Cogumelo { // A classe cogumelo extende de Item, ou seja, possui os mesmos metodos que ela
+	
+	//Sobrescrevendo metodo pegar da classe abstrata item
+		public void pegar() {
+			System.out.println("Pegou Cogumelo");
+		}
+}

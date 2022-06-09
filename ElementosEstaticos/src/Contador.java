a7d5533b38c1d0549d8a79f6580cdba42bb25ab0
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-09 19:40:32 -0300

    Update Contador.java

--- ElementosEstaticos/src/Contador.java
+++ ElementosEstaticos/src/Contador.java
@@ -6,17 +6,17 @@ public class Contador {
 //		this.valor = 10;
 //	}
 	
-	// Aqui foi definido que a variavel vai possui um valor definido que e 1
+	// Aqui foi definido  um valor que e 1
 	static {
 		valor = 1;	
 	}
 
-	// Aqui foi definida uma variavel estatica que vai sempre somar 1, ao valor
+	// Aqui foi definida uma variavel estatica que sempre vai somar 1, ao valor
 	public static void incrementar() {
 		valor ++;
 	}
 
-	// Abaixo esta o getter que vai pegar o valor da variavel valor 
+	// Getter que vai pegar o valor da variavel valor 
 	public static int getValor() {
 		return valor;
 	}

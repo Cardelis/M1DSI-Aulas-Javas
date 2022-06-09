81a7d2170e30cb3a198a5d34a3ca0e612df08c9e
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-09 19:55:24 -0300

    Update Pessoa.java

--- Figurinha/src/Figurinha/Pessoa.java
+++ Figurinha/src/Figurinha/Pessoa.java
@@ -4,7 +4,7 @@ public class Pessoa {
 	// Criando atributos para a classe 
 	String nome;
 	int numFigurinhas;
-	// O atributo x serve para controlar a mensagem que será mostrada
+	// O atributo x serve para controlar a mensagem que serÃ¡ mostrada
 	int x=1;
 	
 	// Metodo que recebe as figurinhas
@@ -15,9 +15,9 @@ public class Pessoa {
 	
 	// Metodo que da as figurinhas
 	void Dar(int numFigurinhas, Pessoa pessoa) {	
-		// Condicao if para impedir que o usuario dê mais figurinhas do que possui
+		//teste condicional para impedir que o usuario de mais figurinhas do que ja tem
 		if(this.numFigurinhas < numFigurinhas) {
-			System.out.println("Você não pode trocar, pois não possui a quantidade necessária de figurinhas \n");
+			System.out.println("VocÃª nÃ£o pode trocar, pois nÃ£o possui a quantidade necessÃ¡ria de figurinhas \n");
 			x=2;
 		}else {
 		this.numFigurinhas -= numFigurinhas;

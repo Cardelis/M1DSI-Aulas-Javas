4f70c9971994c6ac891fd78d3a1cdfcb64b875e2
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-09 19:38:32 -0300

    Update Constantes.java

--- ElementosEstaticos/src/Constantes.java
+++ ElementosEstaticos/src/Constantes.java
@@ -1,13 +1,10 @@
 
 public class Constantes {
-	// Outro metodo de fazer o codigo mais abaixo
-	//	public static final double MEDIA_DA_PROVA = 7.0;   
 	
 	
-	// Final deixa constante
-	public static final double MEDIA_DA_PROVA;
+	public static final double MEDIA_DA_PROVA; // Final deixa constante
 	
-	// Aqui foi definido que a media da prova e uma constante 7, ou seja, nunca vai mudar
+	// Aqui foi definido que a media como 7 e ele nao muda
 	static {
 		MEDIA_DA_PROVA = 7.0;
 	}

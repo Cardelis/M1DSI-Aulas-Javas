922d13925cb4d7198b79297da300f005deaa4227
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-09 19:26:50 -0300

    Update Aplicacao.java

--- Data/src/Aplicacao.java
+++ Data/src/Aplicacao.java
@@ -2,18 +2,19 @@
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		// Instanciando objeto e atribuindo diretamente os valores da data
-		Data d1 = new Data(10, 03, 2000, 0, 30, 10);
-		// Chamando os metodos imprimir tanto de 12 como de 24 horas
+		
+		// Instanciando objeto e atribuindo  valores da data
+		Data d1 = new Data(20, 05, 2010, 0, 30, 10);
+		// Chamando metodos imprimir 
 		d1.imprimir(Data.FORMATO_12H);
 		d1.imprimir(Data.FORMATO_24H);
 		
-		// A mesma coisa foi feita em cima foi feita abaixo
-		Data d2 = new Data(15, 06, 2000, 23, 15, 20);
+		
+		Data d2 = new Data(20, 09, 2010, 23, 15, 20);
 		d2.imprimir(Data.FORMATO_12H);
 		d2.imprimir(Data.FORMATO_24H);
 	
-		Data d3 = new Data(5, 10, 2005);
+		Data d3 = new Data(5, 11, 2006);
 		d3.imprimir(Data.FORMATO_12H);
 		d3.imprimir(Data.FORMATO_24H);
 	}

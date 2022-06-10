7d9c185fa037cdf00697dd3b874922521276f128
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-10 10:35:19 -0300

    Update main.java

--- OrientacaoObjeto/src/main.java
+++ OrientacaoObjeto/src/main.java
@@ -3,27 +3,27 @@ public class main {
 
 	public static void main(String[] args) {
 
-		Pessoa pessoa1 = new Pessoa();
+		Pessoa pessoa1 = new Pessoa(); //Instanciando variavel da classe pessoa
 		System.out.println(pessoa1);
 
-		pessoa1.nome = "Cardelis";
-		pessoa1.numFigurinhas = 1;
+		pessoa1.nome = "Cardelis";// accesando ao atributo por meio de pessoa
+		pessoa1.numFigurinhas = 1;// accesando ao atributo por meio de pessoa
 
-		pessoa1.receber(3);
+		pessoa1.receber(3); // acessando ao metodo receber
 
-		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.nome); // Mostra na tela
 		System.out.println(pessoa1.numFigurinhas);
 
 		Pessoa pessoa2 = new Pessoa();
 
-		pessoa2.nome = "Jonathan";
-		pessoa2.numFigurinhas = 3;
+		pessoa2.nome = "Jonathan"; // accesando ao atributo por meio de pessoa
+		pessoa2.numFigurinhas = 3; // accesando ao atributo por meio de pessoa
 
-		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.nome); // Mostra na tela
 		System.out.println(pessoa2.numFigurinhas);
 
-		boolean retorno = pessoa1.dar(7, pessoa2);
-		if (retorno == true) {
+		boolean retorno = pessoa1.dar(7, pessoa2); // vair retornar um boolean de pessoa1.dar
+		if (retorno == true) { // teste condicional para ver se retorna igual a TRUE
 
 			System.out.println("depois");
 

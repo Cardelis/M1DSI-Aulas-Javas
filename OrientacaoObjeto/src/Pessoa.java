bb483d47a69bbb77f5d0af4f2da53f1c55364d91
Cardelis <102962919+Cardelis@users.noreply.github.com>
2022-06-10 10:38:43 -0300

    Update Pessoa.java

--- OrientacaoObjeto/src/Pessoa.java
+++ OrientacaoObjeto/src/Pessoa.java
@@ -1,18 +1,19 @@
 
 public class Pessoa {
+	// Atributos
 	String nome;
 	int numFigurinhas;
 	
-	void receber(int numfigurinhas) {
-	    this.numFigurinhas += numfigurinhas;
+	void receber(int numfigurinhas) { // metodo receber
+	    this.numFigurinhas += numfigurinhas; // Construtor que vai somar os numFigurinhas e vai guardar o resultado na variavel
 	    
 	}
 	
     boolean dar(int numFigurinha, Pessoa pessoa) {
     // boolean retorno;
     
-    	if(this.numFigurinhas<numFigurinha) { //Condiçao Logica
-    		System.out.println("Aquantidade de figurinhas nao é suficiente para realizar o metodo");
+    	if(this.numFigurinhas<numFigurinha) { //Condiçao Logica 
+    		System.out.println("Aquantidade de figurinhas nao é suficiente para realizar o metodo"); // mostra na tela 
     		// retorno = false;
     		return false; // Retorno boolean: quando uma variavel é
     	}
